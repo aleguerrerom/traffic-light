6b5a6b0b89acfbda7956597758ce8183d0ef2612
Alejandro <Aleguemad94@yahoo.es>
2021-03-02 02:53:48 +0000

    FINAL

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,30 @@
-import React from "react";
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	/// funcion para enviar variale de color y funcion con user state vacio
+	const [ColorSeleccionado, colorFuncion] = useState("");
+	/// variables para quitar seleccion de valores
+	const rojoSeleccionado = ColorSeleccionado == "red" ? "selected" : "";
+	const amarilloSeleccionado =
+		ColorSeleccionado == "yellow" ? "selected" : "";
+	const verdeSeleccionado = ColorSeleccionado == "green" ? "selected" : "";
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div id="cable"></div>
+			<div id="semaforo">
+				<div
+					className={"luz rojo " + rojoSeleccionado}
+					onClick={() => colorFuncion("red")}></div>
+				<div
+					className={"luz amarillo " + amarilloSeleccionado}
+					onClick={() => colorFuncion("yellow")}></div>
+				<div
+					className={"luz verde " + verdeSeleccionado}
+					onClick={() => colorFuncion("green")}></div>
+			</div>
 		</div>
 	);
 }
